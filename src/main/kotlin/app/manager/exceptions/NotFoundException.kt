0a920e7ab58fe6a1263269e00291388104c89c33
cataghiglione <catalina.ghiglione@ing.austral.edu.ar>
2024-06-08 13:01:55 -0300

    added response status

--- src/main/kotlin/app/manager/exceptions/NotFoundException.kt
+++ src/main/kotlin/app/manager/exceptions/NotFoundException.kt
@@ -1,3 +1,7 @@
 package app.manager.exceptions
 
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.ResponseStatus
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND, reason = "Snippet not found")
 class NotFoundException(message: String) : Exception(message)
