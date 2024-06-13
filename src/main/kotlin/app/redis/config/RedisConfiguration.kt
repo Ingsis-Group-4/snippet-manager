8754a7ec9cb6cf6d72a117a6e111d8921b2a27ed
JuanSeZ <juan.zanelli@ing.austral.edu.ar>
2024-06-13 17:20:57 -0300

    add redis to docker compose

--- src/main/kotlin/app/redis/config/RedisConfiguration.kt
+++ src/main/kotlin/app/redis/config/RedisConfiguration.kt
@@ -1,4 +1,5 @@
 
+import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
@@ -12,8 +13,12 @@ class ConnectionFactory(
     @Value("\${redis.host}") private val hostName: String,
     @Value("\${redis.port}") private val port: Int,
 ) {
+
+    private val logger = LoggerFactory.getLogger(ConnectionFactory::class.java)
+
     @Bean
     fun redisConnectionFactory(): LettuceConnectionFactory {
+        logger.info("Connection to redis on host: $hostName and port $port")
         return LettuceConnectionFactory(
             RedisStandaloneConfiguration(hostName, port),
         )
