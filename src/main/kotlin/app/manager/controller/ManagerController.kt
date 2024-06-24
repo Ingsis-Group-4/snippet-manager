82a593b779ab8ca6d58cb48abaf90909c41d52d2
cataghiglione <catalina.ghiglione@ing.austral.edu.ar>
2024-06-24 16:55:35 -0300

    Forgot to add some files to the refactor

--- src/main/kotlin/app/manager/controller/ManagerController.kt
+++ src/main/kotlin/app/manager/controller/ManagerController.kt
@@ -17,14 +17,13 @@ import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @Validated
 class ManagerController(
     @Autowired val managerService: ManagerService,
-) : ManagerControllerSpec{
+) : ManagerControllerSpec {
     @GetMapping("snippets")
     override fun getSnippetsFromUser(
         @AuthenticationPrincipal jwt: Jwt,
